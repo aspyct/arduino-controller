0c7dae72480dd9e1290f9dc6b004064840c254ba
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2013-06-09 21:05:54 +0200

    Created ArduinoController

---
+++ src/main/java/org/aspyct/orchestra/arduino/ArduinoController.java
@@ -0,0 +1,46 @@
+package org.aspyct.orchestra.arduino;
+
+import org.aspyct.orchestra.api.keys.KeysListener;
+import org.aspyct.orchestra.api.keys.KeysMonitor;
+import org.aspyct.orchestra.api.power.PowerListener;
+import org.aspyct.orchestra.api.power.PowerManager;
+
+public class ArduinoController implements PowerManager, KeysMonitor {
+	public void addKeysListener(KeysListener listener) {
+		
+	}
+
+	public void removeKeysListener(KeysListener listener) {
+		
+	}
+
+	public boolean areKeysPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void powerOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void powerOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addPowerListener(PowerListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removePowerListener(PowerListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isPowerOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
